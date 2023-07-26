5871813e9ccef591c55e5d549a4f09f122abe0ef
kannusounshar <kannusoundhar1234@gmail.com>
2023-07-26 21:20:09 +0530

    JS TASK 1

---
+++ script.js
@@ -0,0 +1,24 @@
+// //JavaScript program to swap two variables
+
+// //take input from the users
+// let a = prompt("Enter the first variable: ");
+// let b = prompt("Enter the second variable: ");
+
+// //create a temporary variable
+// let temp;
+
+// //swap variables
+// temp = a;
+// a = b;
+// b = temp;
+
+// console.log(`The value of a after swapping: ${a}`);
+// console.log(`The value of b after swapping: ${b}`);
+
+if ((age = 13)) {
+  console.log("Yes, you are a teenager.");
+} else {
+}
+if (age <= 19) {
+  console.log("No, you are not a teenager.");
+}
